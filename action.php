e48780a5c3f3cb69279a596d75f8372defd9ec81
Jônathas de Almeida Alves Peixoto <peixotoalmeidajonathas@gmail.com>
2025-11-17 20:09:07 -0300

    Adicionando o index e action.

---
+++ action.php
@@ -0,0 +1,26 @@
+<?php
+$texto = $_POST['text'];
+$textoUp = strtoupper($texto);
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Uppercase transformer</title>
+    </head>
+    <body>
+        <p>
+            Bem-vindos ao Uppercase transformer. Transforme qualquer conjunto de letras ou palavras em UM TEXTO CAIXA ALTA!
+        </p>
+        <img src=""/>
+        <h1><-UPPERcase transFORMER-></h1>
+        <form action="action.php" method="POST" target="_blank">  
+            <textarea id="text" name="text" rows="5" cols="50"></textarea><br>
+            <input type="submit" value="UPPAR!">
+        </form>
+        <h2>SEU TEXTO UPPADO, PRONTO PARA SER COPIADO</h2>
+        <textarea id="textuP" name="textuP" rows="5" cols="50">
+            <?php echo $textoUp; ?>
+        </textarea><br>
+    </body>
+</html>
